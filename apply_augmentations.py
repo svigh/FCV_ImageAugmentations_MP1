8e7ce2589b6f44547927e09d25b82186ea69e341
svigh <vigh.sebastian@gmail.com>
2020-10-02 22:57:02 +0300

    Added brighten. Added rotate modes to keep or crop image. Have functions dictionary now.

--- apply_augmentations.py
+++ apply_augmentations.py
@@ -5,12 +5,21 @@ import os
 import sys
 import time
 import argparse
+import math
+from enum import Enum
 
 _DEFINE_USE_TKINTER = False
 
-IMPLEMENTED_OPERATIONS = ["rotate", "abs_tint", "tint", "flip", "blur", "noise"]
 DEFAULT_ROTATION_DEGREES = 15
 DEFAULT_BLUR_KSIZE = 3
+DEFAULT_LOWER_NOISE_LIMIT = 0.7
+DEFAULT_UPPER_NOISE_LIMIT = 1
+DEFAULT_BRIGHTNESS_INTENSITY = 1.5
+class ROTATION_MODES(Enum):
+	KEEP_ORIGINAL_SIZE = 1
+	KEEP_CORNERS = 2
+	CROP_INWARD = 3
+
 
 def parse_args():
 	parser = argparse.ArgumentParser()
@@ -37,10 +46,6 @@ def get_augmentations_from_file(config_file):
 
 				operation = line_contents_list[0].lower()
 				params = [ param.lower() for param in line_contents_list[1:] ]
-
-				if operation not in IMPLEMENTED_OPERATIONS:
-					print("Operation %s not implemented, ignoring" % (operation))
-					continue
 
 				operation_dict = {}
 				operation_dict["operation"] = operation
@@ -107,12 +112,13 @@ def apply_tint(image_path, params, use_absolute_values=False):
 		else:
 			continue
 
-	print("Applied TINT on channels %s on %s" % (params, image_path))
 	return new_image
 
-def apply_rotate(image_path, params):
+# Rotate the image with crop or no crop
+def apply_rotate(image_path, params, crop_type=ROTATION_MODES.KEEP_ORIGINAL_SIZE):
 	new_image = cv2.imread(image_path)
 	height, width = new_image.shape[1::-1]
+
 	try:
 		degrees = int(params[0])
 	except IndexError:
@@ -122,21 +128,39 @@ def apply_rotate(image_path, params):
 	image_center = tuple(np.array([height, width]) / 2)
 	rotation_matrix = cv2.getRotationMatrix2D(image_center, degrees, 1.0)
 
-	# Find the bounding box that contains the rotated image
-	# so that the corners dont get cut
-	abs_cos = abs(rotation_matrix[0][0])
-	abs_sin = abs(rotation_matrix[0][1])
-	bound_w = int(height * abs_sin + width * abs_cos)
-	bound_h = int(height * abs_cos + width * abs_sin)
+	if crop_type == ROTATION_MODES.KEEP_ORIGINAL_SIZE:
+		bound_w, bound_h = height, width
+	elif crop_type == ROTATION_MODES.KEEP_CORNERS:
+		# Find the bounding box that contains the rotated image
+		# so that the corners dont get cut
+		abs_cos = abs(rotation_matrix[0][0])
+		abs_sin = abs(rotation_matrix[0][1])
+		bound_w = int(height * abs_sin + width * abs_cos)
+		bound_h = int(height * abs_cos + width * abs_sin)
+	elif crop_type == ROTATION_MODES.CROP_INWARD:	# TODO: find direct formula for bounding box
+		# Get the bounding box so that the height and width of the
+		# original image are the hypothenuse of each corner triangle
+		# and get the short edge length to eliminate from the bounding
+		# box around the image
+		abs_cos = abs(rotation_matrix[0][0])
+		abs_sin = abs(rotation_matrix[0][1])
+		bound_w = int(height * abs_sin + width * abs_cos)
+		bound_h = int(height * abs_cos + width * abs_sin)
+		bound_w -= int(height * abs_sin) * 2
+		bound_h -= int(width * abs_sin) * 2
+
 	rotation_matrix[0][2] += bound_w/2 - image_center[0]
 	rotation_matrix[1][2] += bound_h/2 - image_center[1]
 
 	new_image = cv2.warpAffine(new_image, rotation_matrix, (bound_w, bound_h), flags=cv2.INTER_LINEAR)
 
-	print("Applied ROTATE %s degrees on %s" % (degrees, image_path))
 	return new_image
 
+# Blur the image with a gausian kernel of size k (bigger kernel for fuzzier result)
 def apply_blur(image_path, params):
+	new_image = cv2.imread(image_path)
+	k_size = 0
+
 	try:
 		k_size = int(params[0])
 		if k_size % 2 == 0:
@@ -146,23 +170,58 @@ def apply_blur(image_path, params):
 		print("Got blur without intensity parameter, defaulting to 3")
 		k_size = DEFAULT_BLUR_KSIZE
 		params.append(str(k_size))	# Update the params to generate a correct output name
-	new_image = cv2.imread(image_path)
 
 	cv2.GaussianBlur(new_image, (k_size, k_size), 0, dst=new_image)
 	return new_image
 
+# Multiply each channel of each pixel with a random amount
 def apply_noise(image_path, params):
 	new_image = cv2.imread(image_path)
-
 	height, width, depth = new_image.shape
-	noise_array = np.random.uniform(low=0.7, high=1, size=(height, width, depth))
 
+	try:
+		lower_noise_limit = float(params[0])
+	except IndexError:
+		lower_noise_limit = DEFAULT_LOWER_NOISE_LIMIT
+		params.append(str(lower_noise_limit))
+
+	try:
+		upper_noise_limit = float(params[1])
+	except IndexError:
+		upper_noise_limit = DEFAULT_UPPER_NOISE_LIMIT
+		params.append(str(upper_noise_limit))
+
+	noise_array = np.random.uniform(
+		low=lower_noise_limit,
+		high=upper_noise_limit,
+		size=(height, width, depth)
+	)
+
+	# Avoid overflow
 	aux_image = new_image * noise_array
 	new_image = aux_image.astype("uint8")
 
 	return new_image
 
+# TODO: maximum amount of brightness for each pixel
+# should be when any channel reaches 255
+# Multiply each channel with intensity
+def apply_brighten(image_path, params):
+	new_image = cv2.imread(image_path)
+	intensity = 0.0
 
+	try:
+		intensity = float(params[0])
+	except IndexError:
+		intensity = DEFAULT_BRIGHTNESS_INTENSITY
+		params.append(str(intensity))
+
+	aux_image = new_image * intensity
+	new_image = aux_image.astype("uint8")
+
+	return new_image
+
+# Flips the image on vertical or horizontal axis
 def apply_flip(image_path, params):
 	new_image = cv2.imread(image_path)
 	axis = []
@@ -175,7 +234,6 @@ def apply_flip(image_path, params):
 			new_image = cv2.flip(new_image, 1)
 			axis.append(flip_axis)
 
-	print("Applied FLIP %s on %s" % (axis, image_path))
 	return new_image
 
 def create_output_dir(output_directory):
@@ -198,18 +256,26 @@ def create_output_dir(output_directory):
 			exit(1)
 
 def apply_augment(input_image_path, augment):
-	if augment["operation"] == "rotate":
-		new_image = apply_rotate(input_image_path, augment["params"])
-	elif augment["operation"] == "abs_tint":
-		new_image = apply_tint(input_image_path, augment["params"], use_absolute_values=True)
-	elif augment["operation"] == "tint":
-		new_image = apply_tint(input_image_path, augment["params"], use_absolute_values=False)
-	elif augment["operation"] == "flip":
-		new_image = apply_flip(input_image_path, augment["params"])
-	elif augment["operation"] == "blur":
-		new_image = apply_blur(input_image_path, augment["params"])
-	elif augment["operation"] == "noise":
-		new_image = apply_noise(input_image_path, augment["params"])
+	# Organizing the functions corresponding to each augment string
+	augments_dictionary = {
+		"rotate" : apply_rotate,
+		"rotate_crop" : lambda image_path, params : apply_rotate(image_path, params, crop_type=ROTATION_MODES.CROP_INWARD),
+		"rotate_keep_size" : lambda image_path, params : apply_rotate(image_path, params, crop_type=ROTATION_MODES.KEEP_ORIGINAL_SIZE),
+		"rotate_resize" : lambda image_path, params : apply_rotate(image_path, params, crop_type=ROTATION_MODES.KEEP_CORNERS),
+		"tint" : apply_tint,
+		"abs_tint" : lambda image_path, params : apply_tint(image_path, params, use_absolute_values=True),
+		"flip" : apply_flip,
+		"blur" : apply_blur,
+		"noise" : apply_noise,
+		"brighten" : apply_brighten
+	}
+
+	try:
+		new_image = augments_dictionary[augment["operation"]](input_image_path, augment["params"])
+	except KeyError:
+		print("Operation %s not implemented, skipping..." % (augment["operation"]))
+		raise
+
 	return new_image
 
 def apply_augments(input_directory, augments, output_directory="Output"):
@@ -227,13 +293,19 @@ def apply_augments(input_directory, augments, output_directory="Output"):
 
 		print("\nAugmenting image %s" % (image_path))
 		for augment in augments:
-			new_image = apply_augment(image_path, augment)
+			try:
+				new_image = apply_augment(image_path, augment)
+			except KeyError:	# In case the augment is not in the augments dictionary
+				continue
 
-			new_image_name = image_basename_no_extension + "_" + augment["operation"] + \
-			  ("-" + "-".join(augment["params"]) if len(augment["params"]) > 0 else "") + "_" + str(augment_index) + "." + image_extension
+			new_image_name = image_basename_no_extension + \
+							  "_" + augment["operation"] + \
+							  ("-" + "-".join(augment["params"]) if len(augment["params"]) > 0 else "") + \
+							  "_" + str(augment_index) + "." + image_extension
+
 			new_image_path = str(os.path.join(output_directory,new_image_name))
 
-			print("\tWriting augmentation to %s" % (new_image_path))
+			print("\tWriting %s augmentation to %s" % (augment["operation"], new_image_path))
 			cv2.imwrite(new_image_path, new_image)
 			augment_index += 1
 
